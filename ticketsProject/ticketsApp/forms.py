dbd0e2bf265cbe35c8cbdaf48194b8278e449019
DiegoB6 <diego.bustamante26@inacapmail.cl>
2024-11-20 14:41:46 -0300

    css

--- ticketsProject/ticketsApp/forms.py
+++ ticketsProject/ticketsApp/forms.py
@@ -27,6 +27,12 @@ class TicketForm(forms.ModelForm):
     estado = forms.ModelChoiceField(queryset=Estado.objects.all())
     area = forms.ModelChoiceField(queryset=Area.objects.all())
 
+    servicio.widget.attrs['class'] = 'form-control'
+    tipo.widget.attrs['class'] = 'form-control'
+    criticidad.widget.attrs['class'] = 'form-control'
+    estado.widget.attrs['class'] = 'form-control'
+    area.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Ticket
         fields = ['detalle', 'observacion', 'servicio', 'tipo', 'criticidad', 'estado', 'area']
@@ -42,6 +48,7 @@ class AreasForms(forms.Form):
 
 class AreasForms(forms.ModelForm):
     area = forms.CharField(label='Area', max_length=25)
+    area.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = Area
@@ -55,6 +62,7 @@ class CriticidadesForms(forms.Form):
 
 class CriticidadesForms(forms.ModelForm):
     criticidad = forms.CharField(label='Criticidad', max_length=25)
+    criticidad.widget.attrs['class'] = 'form-control'
     class Meta:
         model = Criticidad
         fields= '__all__'
@@ -67,6 +75,9 @@ class EstadosForms(forms.Form):
 
 class EstadosForms(forms.ModelForm):
     estado = forms.CharField(label='Estado', max_length=25)
+
+    estado.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Estado
         fields= '__all__'
@@ -79,6 +90,9 @@ class ServiciosForms(forms.Form):
 
 class ServiciosForms(forms.ModelForm):
     servicio = forms.CharField(label='Servicio', max_length=25)
+
+    servicio.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Servicio
         fields= '__all__'
@@ -91,6 +105,9 @@ class TiposForms(forms.Form):
 
 class TiposForms(forms.ModelForm):
     tipo = forms.CharField(label='Tipo', max_length=25)
+
+    tipo.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Tipo
         fields= '__all__'
@@ -101,6 +118,10 @@ class SingUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=150, required=True)
     last_name = forms.CharField(max_length=150, required=False)
     email = forms.EmailField(required=False)
+
+    first_name.widget.attrs['class'] = 'form-control'
+    last_name.widget.attrs['class'] = 'form-control'
+    email.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = User
@@ -115,6 +136,9 @@ class SingUpForm(UserCreationForm):
 
 class CrearStuff(UserCreationForm):
     email = forms.EmailField(required=False)
+
+    email.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
@@ -129,6 +153,8 @@ class CrearStuff(UserCreationForm):
 
 class CrearEjecutivo(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
+
+    password.widget.attrs['class'] = 'form-control'
     
     class Meta:
         model = User
@@ -147,6 +173,15 @@ class TicketConUsuarioForm(forms.ModelForm):
     criticidad = forms.ModelChoiceField(queryset=Criticidad.objects.all())
     estado = forms.ModelChoiceField(queryset=Estado.objects.all())
     area = forms.ModelChoiceField(queryset=Area.objects.all())
+
+    detalle.widget.attrs['class'] = 'form-control'
+    observacion.widget.attrs['class'] = 'form-control'
+    servicio.widget.attrs['class'] = 'form-control'
+    tipo.widget.attrs['class'] = 'form-control'
+    criticidad.widget.attrs['class'] = 'form-control'
+    estado.widget.attrs['class'] = 'form-control'
+    area.widget.attrs['class'] = 'form-control'
+    
 
     class Meta:
         model = Ticket
